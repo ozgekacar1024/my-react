57ead47a2623aabeef39082f64a658b4e29be732
ozgekacar1024 <ozge1024@hotmail.com>
2023-06-19 15:37:51 +0300

    setState3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(){
     super();
     this.state={
-      name:'Özge'
+      name:{firstName: 'Yihua',lastName:'Zhang'},
+      company:'ZTM',
     }
   }
   render(){
@@ -15,12 +16,15 @@ class App extends Component {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi {this.state.name}
+          Hi {this.state.name.firstName}{this.state.name.lastName},I work 
+          at {this.state.company}
         </p>
-       <button>
-        onClick={()=>{
+       <button
+        onClick ={()=>{
          this.setState({name:'Anderi'})//otomatik olarak ismi güncelliyor
         }}
+        >
+          change name
        </button>
       </header>
     </div>

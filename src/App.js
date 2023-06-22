93d6048b09f0fc07b2642d2b50981b11c6b73427
ozgekacar1024 <ozge1024@hotmail.com>
2023-06-22 13:13:33 +0300

    searchBox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg'; //logoyu alıyoryz
 import './App.css'; // burada css kodlarını gelmesini istiyoruz
 import { Component } from 'react';
+import { eventWrapper } from '@testing-library/user-event/dist/utils';
 
-class App extends Component {
+class App extends Component { 
   constructor(){
     super();
     this.state={
-    monsters:[
-      
-    ],
-    };
+    monsters:[],
+    }; 
   }
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((Response) => Response.json())
       //.then((users)=>console.log(users))//kullanıcıların geri çağrımızın argümanı olarak console da aktarılır
-      .then((users)=>this.state(()=>{
-        return{monsters:users}
+      .then((users)=>
+      this.setState(()=>{
+        return{monsters:users} 
         ////monsterin kullanıcıları işaret ettiği nesneyi geri döndürecek
       },
       () => {
@@ -33,14 +33,28 @@ class App extends Component {
     //render html gibi ne gösterileceğini belirler
   return (  //logonun dönmesini sağlıyor
     <div className="App">
+      <input 
+      className='search-box' 
+      type='search' 
+      placeholder='search monsters' 
+      onChange={(event)=>{
+       // console.log(event);
+        console.log(event.target.value);//bu şek,lde canavarlar listesini filtreliyoruz
+      }}
+      /> 
     {this.state.monsters.map((monster) => { 
-      return <div 
-      key={monster.id}><h1>
-        </h1>{monster.name}
+      return (
+      <div key={monster.id}>
+        <h1> {monster.name} </h1>
       </div>
+      );
     })}
+    <button>
+
+    </button>
     </div>
   );
-}}
+}
+}
  
 export default App;
